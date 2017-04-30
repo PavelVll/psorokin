081a83ff80aa42d1a33b637afad761c179ee259c
PavelVll <qunit.83@mail.ru>
2017-04-30 18:37:26 +0300

    task 4087

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ *Class Calculate Класс для выполнения рассчета.
+ *@author Pavel Sorokin
+ *@since 30.04.2017
+ *@version 1
+ */
+public class Calculate {
+
+   /**
+    *Главный метод.
+    *@return "Hello world!"
+    */
+   public static void main(String[] args) {
+        System.out.println("Hello world!");
+   }
+}
